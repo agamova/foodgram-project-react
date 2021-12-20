286d92a574e8e70bdf8dd6fe00622987e319a171
Evgeniya <evgenia@agamova.ru>
2021-12-20 16:43:16 +0300

    deploy

---
+++ backend/users/management/commands/superuser_create.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
+
+User = get_user_model()
+SUPERUSER = settings.SUPERUSER_ADMIN
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if User.objects.count() == 0:
+            admin = User.objects.create_superuser(**SUPERUSER)
+            admin.is_active = True
+            admin.is_admin = True
+            admin.save()
+        else:
+            print('Admin account can only be initialized if no Accounts exist')
